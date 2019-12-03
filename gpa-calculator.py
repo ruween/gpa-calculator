9326086494c6ab63db236cb04ea9f05ad092dcd3
Ruween Iddagoda <48099280+ruween@users.noreply.github.com>
2019-12-03 13:25:18 +0530

    Output starts from 1 now -- GG @kushanjanith

--- gpa-calculator.py
+++ gpa-calculator.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-#Author : Ruween Iddagoda - https://www.linkedin.com/in/ruween-iddagoda-b2400a181
+#Author : Ruween Iddagoda
 
 grading_scale = {"A+" : 4, "A" : 4, "A-" : 3.7, "B+" : 3.3, "B" : 3.0, "B-" : 2.7, "C+" : 2.3, "C" : 2.0, "C-" : 1.7, "D" : 1.3, "D-" : 1.0, "E" : 0 }
 
